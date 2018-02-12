9c658bbce9fb06d13b6dd5ce6f8a76daabed3913
Lanfeng <JIAJIA_CS@QQ.COM>
2018-02-12 11:21:22 +0800

    morgan

---
+++ simple-express/server-2.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var morgan = require('morgan');
+var fs = require('fs');
+var path = require('path')
+
+var hostman = 'localhost';
+var port = '3000';
+
+var app = express();
+
+app.use(morgan('dev', {
+    skip: function (req, res) {
+        return res.statusCode < 400;
+    }
+}));
+
+app.use(morgan('common', {
+    stream: fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a', encoding: 'ascii'})
+}));
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostman, function () {
+    console.log(`server running at http://${hostman}:${port}/`);
+})
